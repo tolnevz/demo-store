58cee817db9805877e0b48158d06a2a7a4d3a887
Anatoly Nevzorov <tolnevz@gmail.com>
2024-06-12 21:53:20 +0300

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './dist',
+    lib: {
+      entry: './src/main.ts',
+      formats: ['es'],
+    },
+  },
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
